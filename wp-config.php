4ec85b1b7d35078ec2b1e153532dc50e21eed357
Marlene Audic <rondineau.m@gmail.com>
2024-03-07 15:20:52 +0100

    Ajout des extensions flying-scripts et wp-super-cache

--- wp-config.php
+++ wp-config.php
@@ -21,6 +21,8 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
+define('WP_CACHE', true);
+define( 'WPCACHEHOME', 'D:\MAMP\htdocs\ChicDressing\wp-content\plugins\wp-super-cache/' );
 define( 'DB_NAME', 'chicdressing' );
 
 /** Utilisateur de la base de données MySQL. */
